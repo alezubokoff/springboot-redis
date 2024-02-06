57cf0bcc0a7099b9a3ca8b5d829165c9817e3000
alexandre.zubokoff <alexandre.zubokoff@picpay.com>
2024-02-06 15:00:48 -0300

    Iniciando implementação de teste de unidade

--- src/main/java/com/zubokoff/demoredis/controller/PersonController.java
+++ src/main/java/com/zubokoff/demoredis/controller/PersonController.java
@@ -6,10 +6,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-
 @RestController
 @RequestMapping("persons")
 @RequiredArgsConstructor
